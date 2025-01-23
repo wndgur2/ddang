b24116f92821a2509ebc54f3944fc1580d5c0147
leejunghyeok <dkandjsl@gmail.com>
2025-01-23 17:41:32 +0900

    docs: fetch 예시 코드

---
+++ src/apis/cat.js
@@ -0,0 +1,14 @@
+// https://api.thecatapi.com/v1/images/search
+/*
+// 20250123174037
+// https://api.thecatapi.com/v1/images/search
+
+[
+  {
+    "id": "b4r",
+    "url": "https://cdn2.thecatapi.com/images/b4r.jpg",
+    "width": 500,
+    "height": 305
+  }
+]
+  */
