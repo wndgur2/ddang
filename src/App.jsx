90e5efa4b794a12da92e4de4dbb045b1df41cbc1
leejunghyeok <wndgur76@naver.com>
2025-01-27 22:20:59 +0900

    fix: PC에서 앱 바깥 배경, page 깨짐 수정

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   initFCM()
 
   return (
-    <div className={`h-screen ${style.container}`}>
+    <div className={`${style.container}`}>
       <Routes>
         <Route path='/explore' element={<ExploreLayout />}>
           <Route index element={<HomePage />} />
