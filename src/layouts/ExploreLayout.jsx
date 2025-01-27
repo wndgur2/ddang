43efc1c49fb62c41615c95d118833eb4a3da7966
leejunghyeok <wndgur76@naver.com>
2025-01-27 18:09:51 +0900

    feat: 기본 패딩 적용

--- src/layouts/ExploreLayout.jsx
+++ src/layouts/ExploreLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import SearchHeader from '../components/headers/SearchHeader'
+import Navbar from '../components/navbar/Navbar'
 
 function ExploreLayout() {
   return (
     <>
       <SearchHeader />
-      <div className='flex flex-col gap-1'>
+      <div className='flex flex-col gap-1 px-4'>
         <Outlet />
       </div>
+      <Navbar />
     </>
   )
 }
