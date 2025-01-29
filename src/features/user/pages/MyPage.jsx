f0c278572373c834f3fcfe60cbd2d5c25f265055
leejunghyeok <wndgur76@naver.com>
2025-01-29 21:57:44 +0900

    feat: font inter, mypage 상대주소로 변경

--- src/features/user/pages/MyPage.jsx
+++ src/features/user/pages/MyPage.jsx
@@ -10,25 +10,25 @@ function MyPage() {
 
   const menus = [
     [
-      { to: '/payment-history', icon: 'credit_card', title: '결제내역' },
-      { to: '/purchase-history', icon: 'sticky_note_2', title: '구매내역' },
-      { to: '/sales-history', icon: 'sticky_note_2', title: '판매내역' },
-      { to: '/favorites', icon: 'favorite', title: '관심목록' },
-      { to: '/gathered-items', icon: 'sort', title: '모아보기' },
-      { to: '/review-history', icon: 'for_you', title: '리뷰내역' },
-      { to: '/my-locations', icon: 'location_on', title: '내 장소' },
+      { to: 'payment-history', icon: 'credit_card', title: '결제내역' },
+      { to: 'purchase-history', icon: 'sticky_note_2', title: '구매내역' },
+      { to: 'sales-history', icon: 'sticky_note_2', title: '판매내역' },
+      { to: 'favorites', icon: 'favorite', title: '관심목록' },
+      { to: 'gathered-items', icon: 'sort', title: '모아보기' },
+      { to: 'review-history', icon: 'for_you', title: '리뷰내역' },
+      { to: 'my-locations', icon: 'location_on', title: '내 장소' },
     ],
     [
-      { to: '/notices', icon: 'event_note', title: '공지사항' },
-      { to: '/customer-service', icon: 'support_agent', title: '고객센터' },
+      { to: 'notices', icon: 'event_note', title: '공지사항' },
+      { to: 'customer-service', icon: 'support_agent', title: '고객센터' },
     ],
     [
       {
-        to: '/terms-and-policies',
+        to: 'terms-and-policies',
         icon: 'sticky_note_2',
         title: '약관 및 정책',
       },
-      { to: '/app-settings', icon: 'settings', title: '앱 설정' },
+      { to: 'app-settings', icon: 'settings', title: '앱 설정' },
     ],
   ]
 
