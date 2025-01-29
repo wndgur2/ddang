30ee6145950c33297f4c430bd4669f26d6a0c2cc
leejunghyeok <wndgur76@naver.com>
2025-01-29 11:52:31 +0900

    test: date-picker in mobile

--- src/components/form/DatePicker.jsx
+++ src/components/form/DatePicker.jsx
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types'
 import { useRef } from 'react'
 import Label from './Label'
+import style from './DatePicker.module.css'
 
 function DatePicker({ label, required }) {
   const dateInput = useRef(null)
 
   return (
-    <div className='flex flex-col gap-2 py-2'>
+    <div className='flex flex-col gap-2 py-2 w-full'>
       {label && <Label text={label} required={required} />}
       <input
         ref={dateInput}
         type='datetime-local'
-        className='flex flex-col border-1 border-gray-300 rounded-md p-3'
+        value='2017-06-01T08:30'
+        className={`flex justify-between border-1 border-gray-300 rounded-md p-3 ${style.datePicker}`}
       />
     </div>
   )
