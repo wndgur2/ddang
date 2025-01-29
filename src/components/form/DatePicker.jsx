2ea026cf50a41065cf76a56496505b43ef6fae88
leejunghyeok <wndgur76@naver.com>
2025-01-29 17:31:52 +0900

    feat: DatePicker 개선

--- src/components/form/DatePicker.jsx
+++ src/components/form/DatePicker.jsx
@@ -1,20 +1,43 @@
 import PropTypes from 'prop-types'
-import { useRef } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import Label from './Label'
-import style from './DatePicker.module.css'
+import MaterialIcon from '../icons/MaterialIcon'
 
 function DatePicker({ label, required }) {
   const dateInput = useRef(null)
+  const [date, setDate] = useState()
+  const koreanLocalDate = useMemo(() => {
+    return date
+      ? new Date(date).toLocaleString('ko-KR', {
+          dateStyle: 'medium',
+          timeStyle: 'short',
+        })
+      : ''
+  }, [date])
 
   return (
     <div className='flex flex-col gap-2 py-2 w-full'>
       {label && <Label text={label} required={required} />}
-      <input
-        ref={dateInput}
-        type='datetime-local'
-        className={`flex justify-between items-center border-1 border-gray-300 rounded-md p-3 ${style.datePicker}`}
-      />
-      <input type='hidden' id='timezone' name='timezone' value='+09:00' />
+      <div
+        className='flex justify-between items-center border-1 border-gray-300 rounded-md p-3'
+        onClick={() => dateInput.current.showPicker()}
+      >
+        {koreanLocalDate ? (
+          <div className='flex gap-2 items-center'>
+            <span className='text-gray-800'>{koreanLocalDate}</span>
+          </div>
+        ) : (
+          <span className='text-gray-500'>선택된 {label} 없음</span>
+        )}
+        <input
+          ref={dateInput}
+          value={date}
+          onChange={e => setDate(e.target.value)}
+          type='datetime-local'
+          className='fixed bottom-0 opacity-0 pointer-events-none'
+        />
+        <MaterialIcon name='calendar_month' />
+      </div>
     </div>
   )
 }
