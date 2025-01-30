46082e45bdd6f86b546a7a2eea9aedfb9d6e0c92
Hyeonji Kang <11qtrm@gmail.com>
2025-01-30 20:51:14 +0900

    feat: 프로필, 신뢰도 컴포넌트 생성

--- src/features/user/components/OtherProfile.jsx
+++ src/features/user/components/OtherProfile.jsx
@@ -1,35 +1,24 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import ProfileImage from './ProfileImage'
-import MaterialIcon from '../../../components/icons/MaterialIcon'
 import TrustScoreBar from './TrustScoreBar'
-import StepTrustBar from './StepTrustBar'
 
 function Profile({ profileSrc, name, trustScore }) {
   return (
     <div className='p-4 bg-white'>
       {/* 프로필 이미지 및 이름 섹션 */}
-      <div className='flex items-center'>
-        <ProfileImage src={profileSrc} size={64} />
-        <div className='ml-4'>
-          <div className='flex items-center space-x-2'>
+      <div className='flex items-center justify-between'>
+        <div className='flex items-center'>
+          <ProfileImage src={profileSrc} size={64} />
+          <div className='ml-4'>
             <h3 className='text-lg font-semibold'>{name}</h3>
-            <Link to='edit-profile'>
-              <button
-                style={{
-                  color: 'var(--color-gray-400)',
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer',
-                }}
-              >
-                <MaterialIcon name='edit' filled>
-                  edit
-                </MaterialIcon>
-              </button>
-            </Link>
           </div>
         </div>
+        <Link to='gathered-items'>
+          <button className='bg-gray-100 text-black py-2 px-4 rounded-lg cursor-pointer'>
+            구독하기
+          </button>
+        </Link>
       </div>
 
       {/* 신뢰도 섹션 */}
@@ -38,10 +27,7 @@ function Profile({ profileSrc, name, trustScore }) {
           <p className='text-ddblue-400 font-bold text-[14px]'>신뢰도</p>
           <span className='text-sm font-bold'>{trustScore}%</span>
         </div>
-        <div className='mt-4'>
-          {/* 5단계 중 현재 3단계 */}
-          <StepTrustBar steps={5} currentStep={3} />
-        </div>
+        <TrustScoreBar trustScore={trustScore} />
       </div>
     </div>
   )
